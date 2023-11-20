200f5ff2364274e573e5cc71eedb1b448ba0fe75
LuisFPrada <128449280+LuisFPrada@users.noreply.github.com>
2023-11-20 18:49:02 -0500

    Actualizacion

--- src/main/java/com/tutorias/skill/forge/academy/controllers/AuthController.java
+++ src/main/java/com/tutorias/skill/forge/academy/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.tutorias.skill.forge.academy.controllers;
 
 import com.tutorias.skill.forge.academy.dao.UsuarioDao;
 import com.tutorias.skill.forge.academy.models.Usuario;
+import com.tutorias.skill.forge.academy.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,17 @@ public class AuthController {
     @Autowired
     private UsuarioDao usuarioDao;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
     public String login(@RequestBody Usuario usuario) {
-        if (usuarioDao.verificarCredenciales(usuario)) {
-            return "OK";
+
+        Usuario usuarioLogueado = usuarioDao.obtenerUsuarioPorCredenciales(usuario);
+        if (usuarioLogueado != null) {
+
+            String tokenJwt = jwtUtil.create(String.valueOf(usuarioLogueado.getId()), usuarioLogueado.getEmail());
+            return tokenJwt;
         }
         return "FAIL";
     }
--- src/main/java/com/tutorias/skill/forge/academy/controllers/UsuarioController.java
+++ src/main/java/com/tutorias/skill/forge/academy/controllers/UsuarioController.java
@@ -4,11 +4,13 @@ package com.tutorias.skill.forge.academy.controllers;
 
 import com.tutorias.skill.forge.academy.dao.UsuarioDao;
 import com.tutorias.skill.forge.academy.models.Usuario;
+import com.tutorias.skill.forge.academy.utils.JWTUtil;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,21 +18,31 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioDao usuarioDao;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
     public Usuario getUsuario(@PathVariable Long id) {
         Usuario usuario = new  Usuario();
         usuario.setId(id);
-        usuario.setNombre("Luis");
-        usuario.setApellido("Prada");
-        usuario.setEmail("luisprada@mail.com");
-        usuario.setTelefono("1234567890");
-        usuario.setTipousuario("Estudiante");
+        usuario.setNombre("");
+        usuario.setApellido("");
+        usuario.setEmail("");
+        usuario.setTelefono("");
+        usuario.setTipousuario("");
         return usuario;
     }
-    @RequestMapping(value = "api/usuarios")
-    public List<Usuario> getUsuarios() {
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
+    public List<Usuario> getUsuarios(@RequestHeader(value="Authorization") String token) {
+        if (!validarToken(token)) {return null;}
+
         return usuarioDao.getUsuarios();
 
+    }
+    private boolean validarToken(String token) {
+        String usuarioId = jwtUtil.getKey(token);
+        return usuarioId != null;
     }
 
     @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
@@ -39,6 +51,7 @@ public class UsuarioController {
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
         String hash = argon2.hash(1, 1024,1, usuario.getPassword());
         usuario.setPassword(hash);
+
         usuarioDao.registrar(usuario);
 
     }
@@ -53,19 +66,10 @@ public class UsuarioController {
         return usuario;
     }
     @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
-    public void eliminar (@PathVariable Long id) {
+    public void eliminar (@RequestHeader(value="Authorization") String token,
+                          @PathVariable Long id) {
+        if (!validarToken(token)) {return;}
         usuarioDao.eliminar(id);
-    }
-
-    @RequestMapping(value = "usuario/34")
-    public Usuario buscar() {
-        Usuario usuario = new  Usuario();
-
-        usuario.setNombre("Luis");
-        usuario.setApellido("Prada");
-        usuario.setEmail("luisprada@mail.com");
-        usuario.setTelefono("1234567890");
-        return usuario;
     }
 
 }
