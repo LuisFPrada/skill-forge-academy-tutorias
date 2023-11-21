4a852fdaab4ac53061db7119521a5de359163e3a
LuisFPrada <128449280+LuisFPrada@users.noreply.github.com>
2023-11-21 17:24:17 -0500

    Version

--- src/main/java/com/tutorias/skill/forge/academy/controllers/UsuarioController.java
+++ src/main/java/com/tutorias/skill/forge/academy/controllers/UsuarioController.java
@@ -20,7 +20,7 @@ public class UsuarioController {
     @Autowired
     private JWTUtil jwtUtil;
 
-    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
     public List<Usuario> getUsuarios(@RequestHeader(value="Authorization") String token) {
         if (!validarToken(token)) { return null; }
 
@@ -33,12 +33,11 @@ public class UsuarioController {
     }
 
     @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
-    public void registrarUsuario(@RequestBody Usuario usuario) {
+    public void registrarUsuarios(@RequestBody Usuario usuario) {
 
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
         String hash = argon2.hash(1, 1024, 1, usuario.getPassword());
         usuario.setPassword(hash);
-
         usuarioDao.registrar(usuario);
     }
 
